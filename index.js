273ef9d600728093d5f54ad6a8d446c472836de8
Bartosz <bartosz.cylwik94@gmail.com>
2022-04-07 01:16:52 +0200

    Revert "add cors"
    This reverts commit 22bd37eff8be54a4b758ae057c039faaf7ead4e5.

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const http = require('http');
 const express = require('express')
-const cors = require('cors')
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-
-
-app.use(cors(corsOptions))
 
 let dataToSimulateDatabase = [
   {
